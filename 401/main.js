2cc640de0cf4b00bbf3460ed7e2806723599633c
EndWall <43109779+EW-EndWall@users.noreply.github.com>
2023-05-06 22:31:17 +0300

    add coming soon, host default, host nameserver and general edit. icon, title static.

--- 401/main.js
+++ 401/main.js
@@ -4,10 +4,14 @@ const lang = navigator.language.split("-")[0];
 fetch("data.json")
   .then((response) => response.json())
   .then(async (data) => {
-    // * default msg
+    // * default title msg
     const defMsg = data[0].msg;
-    // * msg
+    const defTitle = data[0].title;
+    // * title msg
     const msg = (await data.find((e) => e.lang === lang)?.msg) ?? defMsg;
+    const title = (await data.find((e) => e.lang === lang)?.title) ?? defTitle;
+    document.title = title;
+    document.getElementById("title").innerText = title;
     document.getElementById("msg").innerText = msg;
   });
 // * footer domain
